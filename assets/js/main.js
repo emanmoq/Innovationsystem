bbede9f6d9bf91453f034af12de8db307ff5de75
emanmoq <42995029+emanmoq@users.noreply.github.com>
2022-06-17 14:14:29 -0700

    animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,14 @@
 jQuery(function ($) {
+    wow = new WOW(
+        {
+        boxClass:     'wow',      // default
+        animateClass: 'animated', // default
+        offset:       0,          // default
+        mobile:       true,       // default
+        live:         true        // default
+      }
+      )
+      wow.init();
 
     var _initMixedWidget1 = function () {
         var element = document.getElementById("chart1");
