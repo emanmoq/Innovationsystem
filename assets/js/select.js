93d3b0805a28d7d2b9c0fa90b467a472b0d7a7e3
emanmoq <42995029+emanmoq@users.noreply.github.com>
2022-06-24 15:04:51 -0700

    upload

--- assets/js/select.js
+++ assets/js/select.js
@@ -2,8 +2,8 @@ jQuery(function ($) {
         $('#kt_select2_1,#kt_select2_2').select2({
             dir:"rtl"
         });
-        
-      
-
+        $('.kt_datatable_status').select2({
+            dir:"rtl"
+        });
 });
 
